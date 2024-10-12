dd9d65aa5f3bbf391f07f293899183bea4441e1f
GERRIT VISSER <gervisz@live.nl>
2024-10-12 14:06:09 +0200

    Update Stopwatch functions startClock() and stopClock

--- 4/index.js
+++ 4/index.js
@@ -1,33 +1,35 @@
 let secondsElapsed = 0;
 let interval = null;
-const time = document.getElementById("time")
+const time = document.getElementById("time");
 
 function padStart(value) {
-    return String(value).padStart(2, "0")
+  return String(value).padStart(2, "0");
 }
 
 function setTime() {
-    const minutes = Math.floor(secondsElapsed / 60)
-    const seconds = secondsElapsed % 60
-    time.innerHTML = `${padStart(minutes)}:${padStart(seconds)}` ;
+  const minutes = Math.floor(secondsElapsed / 60);
+  const seconds = secondsElapsed % 60;
+  time.innerHTML = `${padStart(minutes)}:${padStart(seconds)}`;
 }
 
 function timer() {
-    secondsElapsed++;
-    setTime()
+  secondsElapsed++;
+  setTime();
 }
 
 function startClock() {
-    if (interval) stopClock()
-    interval = setInterval(timer, 1000)
+  if (!interval) {
+    interval = setInterval(timer, 1000);
+  }
 }
 
 function stopClock() {
-    clearInterval(interval)
+  clearInterval(interval);
+  interval = null;
 }
 
 function resetClock() {
-    stopClock()
-    secondsElapsed = 0;
-    setTime()
+  stopClock();
+  secondsElapsed = 0;
+  setTime();
 }
